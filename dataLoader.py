0ea49f2b21296905eb734fc76f7292ffcdbb44aa
briny-fish <1179102664@qq.com>
2021-05-15 23:43:23 +0800

    新增新数据集的读取以及处理模块

--- dataLoader.py
+++ dataLoader.py
@@ -44,5 +44,19 @@ def readDevDatas():
     print('devdata')
     print(pd.DataFrame(datasdict).shape)
     return datasdict
+
+def read_enzh(file):
+    print(file)
+    datasdict = {'query': [], 'doc': [], 'label': []}
+    cnt = 0
+    for line in open(file, encoding='utf-8'):
+        tmp = line.split('\t')
+        cnt += 1
+        # if(cnt==50000):break
+        datasdict['label'].append(int(tmp[0]))
+        datasdict['query'].append(tmp[1])
+        datasdict['doc'].append(tmp[2][:-1])
+    print(pd.DataFrame(datasdict).shape)
+    return datasdict
 if __name__ == '__main__':
-    readTrainDatas()
+    read_enzh('en-zh.dev')
